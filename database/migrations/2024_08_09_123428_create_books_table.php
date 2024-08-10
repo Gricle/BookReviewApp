e38ded83a9da3f83f25f5c0e25b796add44a5103
Mehdi Kaviani <mehdikavyani02@gmail.com>
2024-08-10 14:46:07 +0330

    Added migrations

---
+++ database/migrations/2024_08_09_123428_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('author_id');
+            $table->foreignId('publisher_id');
+            $table->string('language');
+            $table->string('release_date');
+            $table->string('translator');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
